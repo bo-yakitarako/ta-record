1b32fbb4fc74ff9b8a07b0a96548be59727eea8f
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2023-01-16 15:44:25 +0900

    botの基礎だね

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('わぁい');
+import { Client, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+
+config();
+
+const TOKEN = process.env.BOT_TOKEN as string;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('最強の記録をお前たちに見せてやるよ');
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  if (message.content === 'ping') {
+    await message.reply('pong');
+  }
+});
+
+client.login(TOKEN);
